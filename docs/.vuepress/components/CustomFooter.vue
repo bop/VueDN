737e836f5864dc605c72d1ffcd369aa76c8f71d3
Baptiste <baptiste.ossipow@gmail.com>
2023-10-01 12:21:39 +0200

    footer

--- docs/.vuepress/components/CustomFooter.vue
+++ docs/.vuepress/components/CustomFooter.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="push"></div>
-  <div class="footer center">MIT Licensed | By <a href="https://md.engineer" target="_blank">Stan_MD</a> | AD {{ year }}</div>
+  <div class="footer center">MIT Licensed | By <a href="https://poetic.ch" target="_blank">B. Ossipov</a> &copy; {{ year }}</div>
 </template>
 <script>
 const d = new Date()
